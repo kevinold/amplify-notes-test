1750e2f081b38cc9b93f7341a2655deb5639918b
Kevin Old <kevin@kevinold.com>
2019-09-24 20:27:24 -0500

    add amplify config

---
+++ src/graphql/queries.js
@@ -0,0 +1,53 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getTask = `query GetTask($id: ID!) {
+  getTask(id: $id) {
+    id
+    title
+    description
+    status
+    groups
+  }
+}
+`;
+export const listTasks = `query ListTasks(
+  $filter: ModelTaskFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listTasks(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      title
+      description
+      status
+      groups
+    }
+    nextToken
+  }
+}
+`;
+export const getPrivateNote = `query GetPrivateNote($id: ID!) {
+  getPrivateNote(id: $id) {
+    id
+    content
+    owner
+  }
+}
+`;
+export const listPrivateNotes = `query ListPrivateNotes(
+  $filter: ModelPrivateNoteFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listPrivateNotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      content
+      owner
+    }
+    nextToken
+  }
+}
+`;
